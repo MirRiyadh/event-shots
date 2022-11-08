08e8fea9f765c2c5c5ed4ca527b9ffbe0440c1a3
Riyadh <riyadh.ju@gmail.com>
2022-11-08 20:17:53 +0600

    packages section added

--- src/pages/home/shared/package/Package.js
+++ src/pages/home/shared/package/Package.js
@@ -1,29 +1,35 @@
 import React, { useEffect, useState } from "react";
 import SinglePackage from "./SinglePackage";
+import { FaRegPlusSquare } from "react-icons/fa";
 
 const Package = () => {
   const [packages, setPackages] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/events")
+    fetch("http://localhost:5000/package")
       .then((res) => res.json())
       .then((data) => setPackages(data));
   }, []);
   return (
-    <div>
-      <div className="w-8/12 mx-auto mt-12">
-        <div>
+    <div className="my-12 lg:my-20">
+      <div className=" w-11/12 lg:w-8/12 mx-auto ">
+        <div className="my-8 lg:my-16">
           <p className="text-amber-500 font-semibold">CATEGORIES</p>
-          <h1 className="text-4xl font-semibold text-blue-800">
+          <h1 className="text-5xl font-semibold text-blue-800">
             Popular Packages
           </h1>
         </div>
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-10">
           {packages.map((singlePackage) => (
             <SinglePackage
               key={singlePackage._id}
               singlePackage={singlePackage}
             ></SinglePackage>
           ))}
+        </div>
+        <div className="text-center my-8 lg:my-16 ">
+          <button className="rounded-lg bg-amber-400 px-8 py-2 font-bold hover:bg-amber-500 ">
+            See More
+          </button>
         </div>
       </div>
     </div>
--- src/pages/home/shared/package/SinglePackage.js
+++ src/pages/home/shared/package/SinglePackage.js
@@ -2,7 +2,23 @@ import React from "react";
 
 const SinglePackage = ({ singlePackage }) => {
   const { img, name } = singlePackage;
-  return <div></div>;
+  console.log(singlePackage);
+  return (
+    <div className="card w-full bg-base-100 shadow-sm hover:shadow-2xl transition ease-in-out delay-100 cursor-pointer text-center border">
+      <figure>
+        <img src={img} alt="package" />
+      </figure>
+      <div className="card-body ">
+        <h2 className=" font-semibold text-blue-900 text-2xl  ">{name} </h2>
+
+        <div className="card-actions justify-center">
+          <button className="bg-blue-500 py-2 px-4 text-white font-semibold rounded-lg">
+            All Packages
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePackage;
